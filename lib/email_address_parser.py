2c8af7c14e5812ed214f294a773b7de68e45915e
breechelugui <brendachelugui@gmail.com>
2025-12-02 13:09:16 +0300

    commit all

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,15 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+    
+    def parse(self):
+        # Split by commas and/or spaces
+        parts = re.split(r'[,\s]+', self.email_string)
+        
+        # Filter for valid email addresses using regex
+        emails = [part for part in parts if re.match(r'^[^@]+@[^@]+\.[^@]+$', part)]
+        
+        # Return unique emails sorted alphabetically
+        return sorted(list(set(emails)))
